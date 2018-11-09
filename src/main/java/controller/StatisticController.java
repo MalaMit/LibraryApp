9e821a39fbf36615175600519cc58c90b8823f10
MalaMit <michal.r.motyka@gmail.com>
2018-11-09 20:23:07 +0100

    Initial commit

--- src/main/java/controller/StatisticController.java
+++ src/main/java/controller/StatisticController.java
@@ -2,7 +2,16 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import dao.BookDAO;
+import dao.CatalogueDAO;
+import dao.LendBookDAO;
+import dao.ReaderDAO;
+import dao.ReturnAndExtensionDAO;
+import entities.Catalogue;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -32,7 +41,7 @@ public class StatisticController implements Initializable{
     private HBox lendBookBox;
 
     @FXML
-    private Label borrowLabel;
+    private Label lendLabel;
 
     @FXML
     private HBox holdoverBox;
@@ -40,9 +49,31 @@ public class StatisticController implements Initializable{
     @FXML
     private Label timeOutLabel;
 
+    private BookDAO bookDao = new BookDAO();
+    private CatalogueDAO catalogueDAO = new CatalogueDAO();
+    private ReaderDAO readerDAO = new ReaderDAO();
+    private LendBookDAO lendBookDAO = new LendBookDAO();
+    private ReturnAndExtensionDAO retAndExtenDAO = new ReturnAndExtensionDAO();
+    
+    private Timer timer = new Timer();
+    private TimerTask timerTask = new TimerTask() {
+		
+		@Override
+		public void run() {
+			Platform.runLater(()->{
+				booksLabel.setText(String.valueOf(bookDao.countBook()));
+				copiesLabel.setText(String.valueOf(catalogueDAO.countCopyBook()));
+				accountLabel.setText(String.valueOf(readerDAO.countReader()));
+				lendLabel.setText(String.valueOf(lendBookDAO.countLendBook()));
+				timeOutLabel.setText(String.valueOf(retAndExtenDAO.countBookTimeOut()));
+			});
+			
+		}
+	};
+    
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		timer.schedule(timerTask, 2);
 		
 	}
 
